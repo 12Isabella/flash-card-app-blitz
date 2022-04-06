c59dd3c3a36c0d5a8f9f200f3e9a0b92367a9413
12Isabella <81431189+12Isabella@users.noreply.github.com>
2022-04-06 18:26:51 +0200

    add image, create skeleton, import font awsome

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 import "./App.css";
+import OsloOpera from "./images/OsloOpera.jpg";
 
 function App() {
   return (
     <div className="App">
+      <h1>World Capitals</h1>
+      <img src={OsloOpera} alt="" width="200" />
+      <div>Norway</div>
+      <div>sound</div>
+      <div className="answer-button">
+        <button>
+          <i className="fas fa-smile fa-5x"></i>
+        </button>
+        <button>
+          <i className="fas fa-frown fa-5x"></i>
+        </button>
+      </div>
     </div>
   );
 }
