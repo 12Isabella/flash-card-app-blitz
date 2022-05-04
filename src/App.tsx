94104329dea3433f72e386305e91dbbef9d174c8
Isabella <81431189+12Isabella@users.noreply.github.com>
2022-05-04 17:16:23 +0200

    install react-flip card

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import OsloOpera from "./images/OsloOpera.jpg";
+import ReactCardFlip from "react-card-flip";
 
 function App() {
+  const [isFlipped, setFlipped] = useState(false);
+
+  function flipCard(event: React.MouseEvent<HTMLButtonElement>) {
+    setFlipped(!isFlipped);
+  }
+
   return (
     <div className="App">
       <h1>World Capitals</h1>
       <img src={OsloOpera} alt="" width="200" />
+      <ReactCardFlip isFlipped={isFlipped}>
+        <button onClick={flipCard}>Hey</button>
+        <button onClick={flipCard}>Hello</button>
+      </ReactCardFlip>
       <div>Norway</div>
       <div>sound</div>
       <div className="answer-button">
