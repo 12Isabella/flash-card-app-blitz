ae3c190b8daa7e3a01fc3fd4d7ef2e4b6f47619a
Isabella <81431189+12Isabella@users.noreply.github.com>
2022-05-12 17:30:50 +0200

    fetch data from sanity and set into state, handle empty data

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ import OsloOpera from "./images/OsloOpera.jpg";
 import ReactCardFlip from "react-card-flip";
 import client from "./client";
 
+type Card = {
+  front: string;
+  back: string;
+};
+
 function App() {
+  const [isFlipped, setFlipped] = useState(false);
+  const [cardInfo, setCardInfo] = useState<Card[]>([]);
+
   useEffect(() => {
     client
       .fetch(
@@ -13,21 +21,25 @@ function App() {
       back,
       image{
         asset->{
-          _id,
           url
         },
       },
      
     }`
       )
-      .then((data) => console.log(data))
+      .then((data) => {
+        console.log(data);
+        setCardInfo(data);
+      })
       .catch(console.error);
   }, []);
 
-  const [isFlipped, setFlipped] = useState(false);
-
   function flipCard(event: React.MouseEvent<HTMLDivElement>) {
     setFlipped(!isFlipped);
+  }
+
+  if (cardInfo.length === 0) {
+    return <div>loading...</div>;
   }
 
   return (
@@ -38,11 +50,11 @@ function App() {
         <div className="card" onClick={flipCard}>
           <img src={OsloOpera} alt="" />
 
-          <div className="text">Norway</div>
+          <div className="text">{cardInfo[1].front}</div>
         </div>
         <div className="card" onClick={flipCard}>
           <img src={OsloOpera} alt="" />
-          <div className="text">Oslo</div>
+          <div className="text">{cardInfo[1].back}</div>
         </div>
       </ReactCardFlip>
 
